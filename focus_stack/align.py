127dbb091d7a117e4631fe72b7fb579849a25c28
lucalista <luka.lista@gmail.com>
2024-04-02 21:04:25 +0200

    update

---
+++ focus_stack/align.py
@@ -0,0 +1,136 @@
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+from focus_stack.stack_framework import FramesRefActions
+from termcolor import colored, cprint
+    
+class AlignLayers(FramesRefActions):
+    ALIGN_HOMOGRAPHY = "homography"
+    ALIGN_RIGID = "rigid"
+    def __init__(self, wdir, name, input_path, output_path='', step_align=True, ref_idx=-1, detector='SIFT', descriptor='SIFT', match_method='KNN', flann_idx_kdtree=2, flann_trees=5, flann_checks=50, match_threshold=0.75, method=ALIGN_RIGID, rans_threshold=5.0, plot_matches=False):
+        FramesRefActions.__init__(self, wdir, name, input_path, output_path, ref_idx, step_align)
+        self.detector = detector
+        self.descriptor = descriptor
+        self.match_method = match_method
+        self.flann_idx_kdtree = flann_idx_kdtree
+        self.flann_trees = flann_trees
+        self.flann_checks = flann_checks
+        self.match_threshold = match_threshold
+        self.method = method
+        self.min_matches = 4 if self.method==AlignLayers.ALIGN_HOMOGRAPHY else 3
+        self.rans_threshold = rans_threshold
+        self.plot_matches = plot_matches
+    def run_frame(self, idx, ref_idx):
+        print("aligning frame: {}, file: {}                    ".format(self.count, ref_idx, self.filenames[idx]), end='\r')
+        self.align_images(ref_idx, idx)
+    def create_detector(self):
+        detector = None
+        if self.detector=='SIFT': detector = cv2.SIFT_create()
+        elif self.detector=='ORB': detector = cv2.ORB_create()
+        elif self.detector=='SURF': detector = cv2.FastFeatureDetector_create()
+        elif self.detector=='AKAZE': detector = cv2.AKAZE_create()
+        assert(detector), "Invalid detector: " + self.detector_method
+        return detector
+    def create_descriptor(self):
+        descriptor = None
+        if self.descriptor=='ORB': descriptor = cv2.SIFT_create()
+        elif self.descriptor=='SIFT': descriptor = cv2.ORB_create()
+        elif self.descriptor=='AKAZE': descriptor = cv2.AKAZE_create()
+        assert(descriptor), "Invalid descriptor: " + self.detector_method
+        return descriptor
+    def detect_and_compute(self, detector, descriptor, img_bw_0, img_bw_1):
+        if self.detector==self.descriptor and (self.detector=='SIFT' or self.detector=='AKAZE'):
+            kp_0, des_0 = detector.detectAndCompute(img_bw_0, None)
+            kp_1, des_1 = detector.detectAndCompute(img_bw_1, None)
+        else:
+            kp_0 = detector.detect(img_bw_0, None)
+            kp_1 = detector.detect(img_bw_1, None)
+            kp_0, des_0 = descriptor.compute(img_bw_0, kp_0)
+            kp_1, des_1 = descriptor.compute(img_bw_1, kp_1)
+        return kp_0, kp_1, des_0, des_1
+    def get_good_matches(self, des_0, des_1):
+        if self.match_method=='KNN':
+            flann = cv2.FlannBasedMatcher(dict(algorithm=self.flann_idx_kdtree, trees=self.flann_trees), dict(checks=self.flann_checks))
+            matches = flann.knnMatch(des_0, des_1, k=2)
+            good_matches = []
+            for m, n in matches:
+                if m.distance < self.match_threshold*n.distance:
+                    good_matches.append(m)
+        elif self.match_method=='NORM_HAMMING':
+            bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+            good_matches = bf.match(des_0, des_1)
+            good_matches = sorted(good_matches, key=lambda x: x.distance)
+        return good_matches
+    def find_transform(self, src_pts, dst_pts):
+        if self.method==AlignLayers.ALIGN_HOMOGRAPHY:
+            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, self.rans_threshold)
+        elif self.method==AlignLayers.ALIGN_RIGID:
+            M, mask = cv2.estimateAffinePartial2D(src_pts, dst_pts, method=cv2.RANSAC, ransacReprojThreshold=self.rans_threshold)
+        else:
+            assert(false), "invalid align method: " + self.method
+        return M, mask        
+    def align_images(self, ref_idx, idx):
+        filename_ref, filename_0 = self.filenames[ref_idx], self.filenames[idx]
+        img_0 = cv2.imread(self.input_dir + "/" + filename_0)
+        if img_0 is None: raise Exception("Invalid file: " + self.input_dir + "/" + filename_0)
+        if filename_0 == filename_ref:
+            cv2.imwrite(self.output_dir + "/" + filename_0, img_0, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+            if(self.plot_matches):
+                print('')
+                print("reference image unchanged")
+            return
+        img_ref = cv2.imread((self.output_dir if self.step_process else self.input_dir)  + "/" + filename_ref)
+        if img_ref is None: raise Exception("Invalid file: " + self.input_dir + "/" + filename_ref)    
+        img_bw_0 = cv2.cvtColor(img_0, cv2.COLOR_BGR2GRAY)
+        img_bw_1 = cv2.cvtColor(img_ref, cv2.COLOR_BGR2GRAY)
+        detector = self.create_detector()
+        descriptor = self.create_descriptor()
+        kp_0, kp_1, des_0, des_1 = self.detect_and_compute(detector, descriptor, img_bw_0, img_bw_1)
+        good_matches = self.get_good_matches(des_0, des_1)
+        #if verbose: print("matches: {} ".format(len(good_matches)))
+        n_good_matches = len(good_matches)
+        self.n_matches[idx] = n_good_matches
+        if n_good_matches >= self.min_matches:
+            src_pts = np.float32([kp_0[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+            dst_pts = np.float32([kp_1[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+            M, mask = self.find_transform(src_pts, dst_pts)
+            if(self.plot_matches): matches_mask = mask.ravel().tolist()
+            h, w = img_bw_1.shape
+            pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0] ]).reshape(-1, 1 ,2)
+            if self.method==AlignLayers.ALIGN_HOMOGRAPHY:
+                dst = cv2.perspectiveTransform(pts, M)
+                img_warp = cv2.warpPerspective(img_0, M, (w, h), borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0, 0))
+            elif self.method==AlignLayers.ALIGN_RIGID:
+                img_warp = cv2.warpAffine(img_0, M, (img_0.shape[1], img_0.shape[0]))
+            cv2.imwrite(self.output_dir + "/" + filename_0, img_warp, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+        else:
+            img_warp = None
+            if(self.plot_matches): matches_mask = None
+            cprint("image " + filename_0 + " not aligned, too few matches found: {}         ".format(n_good_matches), "red")
+        if(self.plot_matches):
+            draw_params = dict(matchColor = (0,255,0), singlePointColor=None, matchesMask=matches_mask, flags = 2)
+            img_match = cv2.cvtColor(cv2.drawMatches(img_0, kp_0, img_ref, kp_1, good_matches, None, **draw_params), cv2.COLOR_BGR2RGB)
+            print('')
+            print("matches: {}".format(n_good_matches))
+            plt.figure(figsize=(10, 5))
+            plt.imshow(img_match, 'gray'),
+            plt.show()
+    def begin(self):
+        FramesRefActions.begin(self)
+        self.n_matches = np.zeros(self.counts)
+    def end(self):
+        print("                                 ")
+        plt.figure(figsize=(10, 5))
+        x = np.arange(1, len(self.n_matches) + 1, dtype=int)
+        no_ref = (x != self.ref_idx + 1)
+        x = x[no_ref]
+        y = self.n_matches[no_ref]
+        plt.plot([self.ref_idx + 1, self.ref_idx + 1], [0, y.max()], color='cornflowerblue', linestyle='--', label='reference frame')
+        plt.plot([x[0], x[-1]], [self.min_matches, self.min_matches], color='lightgray', linestyle='--', label='min. matches')
+        plt.plot(x, y, color='navy', label='matches')
+        plt.xlabel('frame')
+        plt.ylabel('n. of matches')
+        plt.legend()
+        plt.ylim(0)
+        plt.xlim(x[0], x[-1])
+        plt.show()
---
+++ focus_stack/balance.py
@@ -0,0 +1,230 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from scipy.optimize import bisect
+from focus_stack.stack_framework import *
+from termcolor import colored, cprint
+
+def gamma_lut(gamma):
+    gamma_inv = 1.0/gamma
+    return np.array([((i/255.0)**gamma_inv)*255 for i in np.arange(0, 256)]).astype("uint8")
+
+def adjust_gamma(image, gamma):
+    return cv2.LUT(image, gamma_lut(gamma))
+
+def adjust_gamma_ch3(image, gamma, ch_range=[0, 1, 2]):
+    chans = cv2.split(image)
+    ch_out = []
+    for c in range(3):
+        if c in ch_range:
+            ch_out.append(cv2.LUT(chans[c], gamma_lut(gamma[c])))
+        else:
+            ch_out.append(chans[c])
+    return cv2.merge(ch_out)
+
+def lumi_expect(hist, gamma, i_min=0, i_max=255):
+    return np.average(gamma_lut(gamma)[i_min:i_max+1], weights=hist.flatten()[i_min:i_max+1])
+
+def lumi_mask(image, mask_radius=-1):
+    if mask_radius > 0:
+        height, width, channels = image.shape
+        mask = np.zeros(image.shape[:2], dtype="uint8")
+        cv2.circle(mask, (width//2, height//2), int(min(width, height)*mask_radius/2),  (255, 255, 255), -1)
+    else:
+        mask = np.full(image.shape[:2], 266, dtype="uint8")
+    return mask
+
+def histo_plot(ax, histo, x_label, color):
+    ax.set_ylabel("# of Pixels")
+    ax.set_xlabel(x_label)
+    ax.set_xlim([0, 256])
+    ax.set_yscale('log')
+    ax.plot(histo, color=color)
+
+class BalanceLayers(FramesRefActions):
+    def __init__(self, wdir, name, input_path, output_path='', ref_idx=-1, mask_radius=-1, i_min=0, i_max=255, plot_histograms=False):
+        FramesRefActions.__init__(self, wdir, name, input_path, output_path, ref_idx, step_process=False)
+        self.mask_radius = mask_radius
+        self.i_min = i_min
+        self.i_max = i_max
+        self.plot_histograms = plot_histograms
+    def run_frame(self, idx, ref_idx):
+        print("balancing frame: {}, file: {}                    ".format(self.count, idx, self.filenames[idx]), end='\r')
+        self.balance(idx)
+    def begin(self):
+        FramesRefActions.begin(self)
+        self.image_ref = self.preprocess(cv2.imread(self.input_dir + "/" + self.filenames[self.ref_idx]))
+        self.mean_ref, self.hist_ref = self.get_histos(self.image_ref)
+    def end(self):
+        print("                                 ")
+    def balance(self, idx):
+        image = cv2.imread(self.input_dir + "/" + self.filenames[idx])
+        if(idx != self.ref_idx):
+            image = self.preprocess(image)
+            image = self.adjust_gamma(image)
+            image = self.postprocess(image)
+        cv2.imwrite(self.output_dir + "/" + self.filenames[idx], image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+    def preprocess(self, image):
+        return image
+    def postprocess(self, image):
+        return image
+    def get_histos(self, image):
+        assert(False), 'abstract method'
+    def adjust_gamma(self, image):
+        assert(False), 'abstract method'
+    
+class BalanceLayersLumi(BalanceLayers):
+    def __init__(self, wdir, name, input_path, output_path='', ref_idx=-1, mask_radius=-1, i_min=0, i_max=255, plot_histograms=False):
+        BalanceLayers.__init__(self, wdir, name, input_path, output_path, ref_idx, mask_radius, i_min, i_max, plot_histograms)
+    def get_histos(self, image):
+        mask = lumi_mask(image, self.mask_radius)
+        hist_lumi = cv2.calcHist([cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)], [0], mask, [256], [0, 256])
+        if self.plot_histograms:
+            chans = cv2.split(image)
+            colors = ("r", "g", "b")
+            fig, axs = plt.subplots(1, 2, figsize=(6, 2), sharey=True)
+            histo_plot(axs[0], hist_lumi, "pixel luminosity", 'black')
+            for (chan, color) in zip(chans, colors):
+                hist_col = cv2.calcHist([chan], [0], mask, [256], [0, 256])
+                histo_plot(axs[1], hist_col, "r,g,b luminosity", color)
+            plt.show()
+        mean_lumi = np.average(list(range(256))[self.i_min:self.i_max + 1], weights=hist_lumi.flatten()[self.i_min:self.i_max + 1])
+        return mean_lumi, hist_lumi
+    def adjust_gamma(self, image):
+        mean, hist = self.get_histos(image)
+        f = lambda x: lumi_expect(hist, x, self.i_min, self.i_max) - self.mean_ref
+        gamma = bisect(f, 0.1, 5)
+        self.gamma[self.count - 1] = gamma
+        return adjust_gamma(image, gamma)
+    def begin(self):
+        BalanceLayers.begin(self)
+        self.gamma = np.ones(self.counts)
+    def end(self):
+        BalanceLayers.end(self)
+        plt.figure(figsize=(10, 5))
+        x = np.arange(1, len(self.gamma) + 1, dtype=int)
+        y = self.gamma
+        plt.plot([self.ref_idx + 1, self.ref_idx + 1], [0, 1], color='cornflowerblue', linestyle='--', label='reference frame')
+        plt.plot([x[0], x[-1]], [1, 1], color='lightgray', linestyle='--', label='no correction')
+        plt.plot(x, y, color='navy', label='luminosity gamma correction')
+        plt.xlabel('frame')
+        plt.ylabel('gamma correction')
+        plt.legend()
+        plt.xlim(x[0], x[-1])
+        plt.ylim(0)
+        plt.show()
+        
+class BalanceLayersRGB(BalanceLayers):
+    def __init__(self, wdir, name, input_path, output_path='', ref_idx=-1, mask_radius=-1, i_min=0, i_max=255, plot_histograms=False):
+        BalanceLayers.__init__(self, wdir, name, input_path, output_path, ref_idx, mask_radius, i_min, i_max, plot_histograms)
+    def get_histos(self, image):
+        mask = lumi_mask(image, self.mask_radius)
+        hist = []
+        mean = []
+        chans = cv2.split(image)
+        colors = ("r", "g", "b")
+        for (chan, color) in zip(chans, colors):
+            hist.append(cv2.calcHist([chan], [0], mask, [256], [0, 256]))
+            mean.append(np.average(list(range(256))[self.i_min:self.i_max + 1], weights=hist[-1].flatten()[self.i_min:self.i_max + 1]))
+        if self.plot_histograms:
+            fig, axs = plt.subplots(1, 3, figsize=(6, 2), sharey=True)
+            for c in [2, 1, 0]:
+                histo_plot(axs[c], hist[c], colors[c] + " luminosity", colors[c])
+            plt.show()
+        return mean, hist
+    def adjust_gamma(self, image):
+        mean, hist = self.get_histos(image)
+        gamma = []
+        for c in range(3):
+            f = lambda x: lumi_expect(hist[c], x, self.i_min, self.i_max) - self.mean_ref[c]
+            gamma.append(bisect(f, 0.1, 5))
+        self.gamma[self.count - 1] = gamma
+        return adjust_gamma_ch3(image, gamma)
+    def begin(self):
+        BalanceLayers.begin(self)
+        self.gamma = np.ones((self.counts, 3))
+    def end(self):
+        BalanceLayers.end(self)
+        plt.figure(figsize=(10, 5))
+        x = np.arange(1, len(self.gamma) + 1, dtype=int)
+        y = self.gamma
+        plt.plot([self.ref_idx + 1, self.ref_idx + 1], [0, 1], color='cornflowerblue', linestyle='--', label='reference frame')
+        plt.plot([x[0], x[-1]], [1, 1], color='lightgray', linestyle='--', label='no correction')
+        plt.plot(x, y[:, 0], color='r', label='R gamma correction')
+        plt.plot(x, y[:, 1], color='g', label='G gamma correction')
+        plt.plot(x, y[:, 2], color='b', label='B gamma correction')
+        plt.xlabel('frame')
+        plt.ylabel('gamma correction')
+        plt.legend()
+        plt.xlim(x[0], x[-1])
+        plt.ylim(0)
+        plt.show()
+    
+class BalanceLayersCh2(BalanceLayers):
+    def __init__(self, wdir, name, input_path, output_path='', ref_idx=-1, mask_radius=-1, i_min=0, i_max=255, plot_histograms=False):
+        BalanceLayers.__init__(self, wdir, name, input_path, output_path, ref_idx, mask_radius, i_min, i_max, plot_histograms)
+    def preprocess(self, image):
+        assert(False), 'abstract method'
+    def get_labels(self):
+        assert(False), 'abstract method'
+    def get_histos(self, image):
+        mask = lumi_mask(image, self.mask_radius)
+        hist = []
+        mean = []
+        chans = cv2.split(image)
+        for (chan, color) in zip(chans, self.colors):
+            hist.append(cv2.calcHist([chan], [0], mask, [256], [0, 256]))
+            mean.append(np.average(list(range(256))[self.i_min:self.i_max + 1], weights=hist[-1].flatten()[self.i_min:self.i_max + 1]))
+        if self.plot_histograms:
+            fig, axs = plt.subplots(1, 3, figsize=(6, 2), sharey=True)
+            for c in range(3):
+                histo_plot(axs[c], hist[c], self.labels[c], colors[c])
+            plt.show()
+        return mean, hist
+    def adjust_gamma(self, image):
+        mean, hist = self.get_histos(image)
+        gamma = [1, 1, 1]
+        ch_range = [1, 2]
+        for c in ch_range:
+            f = lambda x: lumi_expect(hist[c], x,self. i_min, self.i_max) - self.mean_ref[c]
+            gamma[c] = bisect(f, 0.1, 5)
+        self.gamma[self.count - 1] = gamma[1:]
+        return adjust_gamma_ch3(image, gamma, ch_range)
+    def begin(self):
+        BalanceLayers.begin(self)
+        self.gamma = np.ones((self.counts, 2))
+    def end(self):
+        BalanceLayers.end(self)
+        plt.figure(figsize=(10, 5))
+        x = np.arange(1, len(self.gamma) + 1, dtype=int)
+        y = self.gamma
+        plt.plot([self.ref_idx + 1, self.ref_idx + 1], [0, 1], color='cornflowerblue', linestyle='--', label='reference frame')
+        plt.plot([x[0], x[-1]], [1, 1], color='lightgray', linestyle='--', label='no correction')
+        plt.plot(x, y[:, 0], color=self.colors[1], label=self.labels[1] + ' gamma correction')
+        plt.plot(x, y[:, 1], color=self.colors[2], label=self.labels[2] + ' gamma correction')
+        plt.xlabel('frame')
+        plt.ylabel('gamma correction')
+        plt.legend()
+        plt.xlim(x[0], x[-1])
+        plt.ylim(0)
+        plt.show()
+        
+class BalanceLayersSV(BalanceLayersCh2):
+    def __init__(self, wdir, name, input_path, output_path='', ref_idx=-1, mask_radius=-1, i_min=0, i_max=255, plot_histograms=False):
+        BalanceLayersCh2.__init__(self, wdir, name, input_path, output_path, ref_idx, mask_radius, i_min, i_max, plot_histograms)
+        self.labels = ("H", "S", "V")
+        self.colors = ("hotpink", "forestgreen", "navy")
+    def preprocess(self, image):
+        return cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    def postprocess(self, image):
+        return cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+    
+class BalanceLayersLS(BalanceLayersCh2):
+    def __init__(self, wdir, name, input_path, output_path='', ref_idx=-1, mask_radius=-1, i_min=0, i_max=255, plot_histograms=False):
+        BalanceLayersCh2.__init__(self, wdir, name, input_path, output_path, ref_idx, mask_radius, i_min, i_max, plot_histograms)
+        self.labels = ("H", "L", "S")
+        self.colors = ("hotpink", "navy", "forestgreen")
+    def preprocess(self, image):
+        return cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
+    def postprocess(self, image):
+        return cv2.cvtColor(image, cv2.COLOR_HLS2BGR)
