a938b3b94783d20f98c99ee04b028ae631441eeb
lucalista <luka.lista@gmail.com>
2025-02-21 17:52:32 +0100

    optimize

--- focus_stack/pyramid.py
+++ focus_stack/pyramid.py
@@ -4,60 +4,54 @@ import cv2
 from termcolor import colored
 from scipy.ndimage import generic_filter
 
-def generating_kernel(a):
-    kernel = np.array([0.25 - a/2.0, 0.25, a, 0.25, 0.25 - a/2.0])
-    return np.outer(kernel, kernel)
-
-def convolve(image, kernel=generating_kernel(0.4)):
-    return cv2.filter2D(image, -1, kernel, borderType=cv2.BORDER_REFLECT101)
-    
-def reduce_layer(layer, kernel):
-    if len(layer.shape) == 2:
-        convolution = convolve(layer, kernel)
-        return convolution[::2, ::2]
-    ch_layer = reduce_layer(layer[:, :, 0], kernel)
-    next_layer = np.zeros(list(ch_layer.shape) + [layer.shape[2]], dtype = ch_layer.dtype)
-    next_layer[:, :, 0] = ch_layer
-    for channel in range(1, layer.shape[2]):
-        next_layer[:, :, channel] = reduce_layer(layer[:, :, channel], kernel)
-    return next_layer
-
-def expand_layer(layer, kernel):
-    if len(layer.shape) == 2:
-        expand = np.zeros((2*layer.shape[0], 2*layer.shape[1]), dtype=np.float64)
-        expand[::2, ::2] = layer;
-        convolution = convolve(expand, kernel)
-        return 4.*convolution
-    ch_layer = expand_layer(layer[:, :, 0], kernel)
-    next_layer = np.zeros(list(ch_layer.shape) + [layer.shape[2]], dtype=ch_layer.dtype)
-    next_layer[:, :, 0] = ch_layer
-    for channel in range(1, layer.shape[2]):
-        next_layer[:, :, channel] = expand_layer(layer[:, :, channel], kernel)
-    return next_layer
-
 class PyramidStack:
     def __init__(self, min_size=32, kernel_size=5, gen_kernel=0.4):
         self.min_size = min_size
         self.kernel_size = kernel_size
         self.pad_amount = int((self.kernel_size - 1)/2)
         self.offset = np.arange(-self.pad_amount, self.pad_amount + 1)
-        self.gen_kernel = generating_kernel(gen_kernel)
+        kernel = np.array([0.25 - gen_kernel/2.0, 0.25, gen_kernel, 0.25, 0.25 - gen_kernel/2.0])
+        self.gen_kernel = np.outer(kernel, kernel)
     def messenger(self, messenger):
         self.messenger = messenger
     def print_message(self, msg):
         self.messenger.sub_message(colored(msg, "light_blue"), end='\r')
-    def gaussian_pyramid(self, images, levels, kernel=generating_kernel(0.4)):
+    def convolve(self, image):
+        return cv2.filter2D(image, -1, self.gen_kernel, borderType=cv2.BORDER_REFLECT101)
+    def reduce_layer(self, layer):
+        if len(layer.shape) == 2:
+            convolution = self.convolve(layer)
+            return convolution[::2, ::2]
+        ch_layer = self.reduce_layer(layer[:, :, 0])
+        next_layer = np.zeros(list(ch_layer.shape) + [layer.shape[2]], dtype = ch_layer.dtype)
+        next_layer[:, :, 0] = ch_layer
+        for channel in range(1, layer.shape[2]):
+            next_layer[:, :, channel] = self.reduce_layer(layer[:, :, channel])
+        return next_layer
+    def expand_layer(self, layer):
+        if len(layer.shape) == 2:
+            expand = np.zeros((2*layer.shape[0], 2*layer.shape[1]), dtype=np.float64)
+            expand[::2, ::2] = layer;
+            convolution = self.convolve(expand)
+            return 4.*convolution
+        ch_layer = self.expand_layer(layer[:, :, 0])
+        next_layer = np.zeros(list(ch_layer.shape) + [layer.shape[2]], dtype=ch_layer.dtype)
+        next_layer[:, :, 0] = ch_layer
+        for channel in range(1, layer.shape[2]):
+            next_layer[:, :, channel] = self.expand_layer(layer[:, :, channel])
+        return next_layer
+    def gaussian_pyramid(self, images, levels):
         self.print_message(' - begin gaussian pyramids ')
         pyramid = [images.astype(np.float64)]
         num_images = images.shape[0]
         while levels > 0:
             self.print_message(' - gaussian pyramids, level: {} '.format(levels))
-            next_layer = reduce_layer(pyramid[-1][0], kernel)
+            next_layer = self.reduce_layer(pyramid[-1][0])
             next_layer_size = [num_images] + list(next_layer.shape)
             pyramid.append(np.zeros(next_layer_size, dtype=next_layer.dtype))
             pyramid[-1][0] = next_layer
             for layer in range(1, images.shape[0]):
-                pyramid[-1][layer] = reduce_layer(pyramid[-2][layer], kernel)
+                pyramid[-1][layer] = self.reduce_layer(pyramid[-2][layer])
             levels = levels - 1
         self.print_message(' - gaussian pyramids completed ')
         return pyramid
@@ -70,7 +64,7 @@ class PyramidStack:
             pyramid.append(np.zeros(gauss.shape, dtype=gauss.dtype))
             for layer in range(images.shape[0]):
                 gauss_layer = gauss[layer]
-                expanded = expand_layer(gaussian[level][layer], self.gen_kernel)
+                expanded = self.expand_layer(gaussian[level][layer])
                 if expanded.shape != gauss_layer.shape:
                     expanded = expanded[:gauss_layer.shape[0], :gauss_layer.shape[1]]
                 pyramid[-1][layer] = gauss_layer - expanded
@@ -114,7 +108,7 @@ class PyramidStack:
         layers = laplacians.shape[0]
         region_energies = np.zeros(laplacians.shape[:3], dtype=np.float64)
         gray_laps = [cv2.cvtColor(laplacians[layer].astype(np.float32), cv2.COLOR_BGR2GRAY) for layer in range(layers)]
-        region_energies = np.array([convolve(np.square(gray_lap)) for gray_lap in gray_laps])
+        region_energies = np.array([self.convolve(np.square(gray_lap)) for gray_lap in gray_laps])
         best_re = np.argmax(region_energies, axis = 0)
         self.print_message(' - compute fused laplacian ')
         fused = np.zeros(laplacians.shape[1:], dtype=laplacians.dtype)
@@ -133,7 +127,7 @@ class PyramidStack:
     def collapse(self, pyramid):
         image = pyramid[-1]
         for layer in pyramid[-2::-1]:
-            expanded = expand_layer(image, self.gen_kernel)
+            expanded = self.expand_layer(image)
             if expanded.shape != layer.shape:
                 expanded = expanded[:layer.shape[0], :layer.shape[1]]
             image = expanded + layer
@@ -146,6 +140,6 @@ class PyramidStack:
         smallest_side = min(images[0].shape[:2])
         depth = int(np.log2(smallest_side/self.min_size))
         pyramids = self.laplacian_pyramid(images, depth)
-        fusion = self. fuse_pyramids(pyramids)
+        fusion = self.fuse_pyramids(pyramids)
         stacked_image = self.collapse(fusion)
         return np.clip(np.absolute(stacked_image), 0, self.n_values - 1).astype(self.dtype)
