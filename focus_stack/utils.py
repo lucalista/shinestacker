484df81c229ee7a8c2d2a7368e115eb94fc4ad54
Luca Lista <luca.lista@infn.it>
2025-04-28 18:44:31 +0200

    update

--- focus_stack/utils.py
+++ focus_stack/utils.py
@@ -6,8 +6,8 @@ from PIL.ExifTags import TAGS
 import warnings
 
 # Bad TIFF keys in specific PIL version
-# BAD_EXIF_KEYS_16BITS_TIFF = [33723, 34665]
-BAD_EXIF_KEYS_16BITS_TIFF = []
+BAD_EXIF_KEYS_16BITS_TIFF = [33723, 34665]
+#BAD_EXIF_KEYS_16BITS_TIFF = []
 
 def check_path_exists(path):
     assert os.path.exists(path), 'path does not exist: ' + path
@@ -25,8 +25,10 @@ def write_img(file_path, img):
     ext = file_path.split(".")[-1]
     if ext == 'jpeg' or ext == 'jpg':
         cv2.imwrite(file_path, img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-    elif ext == 'tiff' or ext == 'tif' or ext == 'png':
+    elif ext == 'tiff' or ext == 'tif':
         cv2.imwrite(file_path, img)
+    elif ext == 'png':
+        cv2.imwrite(file_path, img [int(cv.IMWRITE_TIFF_COMPRESSION), 1]) 
         
 def img_8bit(img):
     if img.dtype == np.uint16:
