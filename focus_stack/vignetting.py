7f54c302a2edcb6f38fd84d6f0609458f1e77b3b
Luca Lista <luca.lista@infn.it>
2025-05-25 08:03:59 +0200

    update

--- focus_stack/vignetting.py
+++ focus_stack/vignetting.py
@@ -7,18 +7,19 @@ import logging
 
 
 class Vignetting:
-    def __init__(self, r_steps=100, black_threshold=1, apply_correction=True):
+    def __init__(self, r_steps=100, black_threshold=1, apply_correction=True, plot_histograms=False):
         self.r_steps = r_steps
         self.black_threshold = black_threshold
         self.apply_correction = apply_correction
+        self.plot_histograms = plot_histograms
 
     def radial_mean_intensity(self, image):
         if len(image.shape) > 2:
             raise ValueError("The image must be grayscale")
         h, w = image.shape
         center = (w / 2, h / 2)
-        r_max = np.sqrt((w / 2)**2 + (h / 2)**2)
-        radii = np.linspace(0, r_max, self.r_steps + 1)
+        self.r_max = np.sqrt((w / 2)**2 + (h / 2)**2)
+        radii = np.linspace(0, self.r_max, self.r_steps + 1)
         mean_intensities = np.zeros(self.r_steps)
         y, x = np.ogrid[:h, :w]
         dist_from_center = np.sqrt((x - center[0])**2 + (y - center[1])**2)
@@ -58,19 +59,30 @@ class Vignetting:
         i0_fit, k_fit, r0_fit = self.fit_sigmoid(radii, intensities)
         self.process.sub_message(f": fit parameters: i0={i0_fit:.2f}, k={k_fit:.4f}, r0={r0_fit:.2f}",
                                  level=logging.DEBUG)
-        plt.figure(figsize=(10, 5))
-        plt.plot(radii, intensities, label="image mean intensity")
-        plt.plot(radii, Vignetting.sigmoid(radii, i0_fit, k_fit, r0_fit), label="sigmoid fit")
-        plt.xlabel('radius (pixels)')
-        plt.ylabel('mean intensity')
-        plt.legend()
-        plt.xlim(radii[0], radii[-1])
-        plt.ylim(0)
-        save_plot(self.process.plot_path + "/" + self.process.name + "-radial-intensity-{:04d}.pdf".format(idx))
+        if self.plot_histograms:
+            plt.figure(figsize=(10, 5))
+            plt.plot(radii, intensities, label="image mean intensity")
+            plt.plot(radii, Vignetting.sigmoid(radii, i0_fit, k_fit, r0_fit), label="sigmoid fit")
+            plt.xlabel('radius (pixels)')
+            plt.ylabel('mean intensity')
+            plt.legend()
+            plt.xlim(radii[0], radii[-1])
+            plt.ylim(0)
+            save_plot(self.process.plot_path + "/" + self.process.name + "-radial-intensity-{:04d}.pdf".format(idx))
+        self.r0s.append(r0_fit)
         return self.correct_vignetting(img_0, (i0_fit, k_fit, r0_fit)) if self.apply_correction else img_0
 
     def begin(self, process):
         self.process = process
+        self.r0s = []
 
     def end(self):
-        pass
+        plt.figure(figsize=(10, 5))
+        xs = np.arange(1, len(self.r0s) + 1, dtype=int)
+        plt.plot(xs, self.r0s, label="sigmoid $r_0$ parameter")
+        plt.xlabel('frame')
+        plt.ylabel('$r_0$ (pixels)')
+        plt.legend()
+        plt.xlim(xs[0], xs[-1])
+        plt.ylim(0, self.r_max)
+        save_plot(self.process.plot_path + "/" + self.process.name + "-r0.pdf")
