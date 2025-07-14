3083ea0dc4052949e193ba97b103af9463d5431a
Luca Lista <luca.lista@infn.it>
2025-07-14 13:20:43 +0200

    simplified naming for release package [ci skip]

--- scripts/build_release.py
+++ scripts/build_release.py
@@ -10,48 +10,38 @@ import platform
 os.chdir("../")
 project_root = Path(__file__).resolve().parent.parent
 dist_dir = project_root / "dist"
+project_name = "focusstack"
+app_name = "focusstack"
+package_dir = "focusstack"
 
 sys_name = platform.system().lower()
+
+pyinstaller_cmd = ["pyinstaller", "--onedir", f"--name={app_name}", "--paths=src",
+                   f"--distpath=dist/{package_dir}", f"--collect-all={project_name}"]
 if sys_name == 'darwin':
-    pyinstaller_cmd = ["pyinstaller", "--windowed", "--onedir", "--name=focusstack-main", "--paths=src",
-                       "--collect-all=focusstack", "--icon=ico/focus_stack.icns"]
+    pyinstaller_cmd += ["--windowed", "--icon=ico/focus_stack.icns"]
 elif sys_name == 'windows':
-    pyinstaller_cmd = ["pyinstaller", "--windowed", "--onedir", "--name=focusstack-main", "--paths=src",
-                       "--collect-all=focusstack", "--icon=ico/focus_stack.ico"]
-else:
-    pyinstaller_cmd = ["pyinstaller", "--onedir", "--name=focusstack-main", "--paths=src",
-                       "--collect-all=focusstack"]
+    pyinstaller_cmd += ["--windowed", "--icon=ico/focus_stack.ico"]
+pyinstaller_cmd += ["src/focusstack/app/main.py"]
 
-pyinstaller_cmd.append("src/focusstack/app/main.py")
-
+print(" ".join(pyinstaller_cmd))
 subprocess.run(pyinstaller_cmd, check=True)
 
 ico_dir = project_root / "ico"
-target_ico = dist_dir / "focusstack-main" / "ico"
+target_ico = dist_dir / package_dir / "ico"
 if target_ico.exists():
     shutil.rmtree(target_ico)
 shutil.copytree(ico_dir, target_ico)
 
 examples_dir = project_root / "examples"
-target_examples = dist_dir / "focusstack-main" / "examples"
+target_examples = dist_dir / package_dir / "examples"
 target_examples.mkdir(exist_ok=True)
 for project_file in ["project.fsp", "stack-from-frames.fsp"]:
     shutil.copy(examples_dir / project_file, target_examples)
-
-if sys_name == 'darwin':
-    app_dir = dist_dir / "focusstack-main.app"
-    target_app = dist_dir / "focusstack-main"
-    if target_app.exists():
-        shutil.move(app_dir, target_app)
-elif sys_name == 'windows':
-    app_dir = dist_dir / "focusstack-main.exe"
-    target_app = dist_dir / "focusstack-main"
-    if target_app.exists():
-        shutil.move(app_dir, target_app)
 
 shutil.make_archive(
     base_name=str(dist_dir / "focusstack-release"),
     format="zip",
     root_dir=dist_dir,
-    base_dir="focusstack-main"
+    base_dir=package_dir
 )
