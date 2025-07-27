41fc55803c80139c873f7e0ae475e4c2597c24f4
Luca Lista <luca.lista@infn.it>
2025-07-27 18:48:46 +0200

    updated doc and public exports

--- src/shinestacker/core/__init__.py
+++ src/shinestacker/core/__init__.py
@@ -2,7 +2,10 @@
 from .logging import setup_logging
 from .exceptions import (FocusStackError, InvalidOptionError, ImageLoadError, ImageSaveError, AlignmentError,
                          BitDepthError, ShapeError, RunStopException)
+from .framework import Job
 
 __all__ = [
-    'setup_logging', 'FocusStackError', 'InvalidOptionError', 'ImageLoadError', 'ImageSaveError',
-    'AlignmentError','BitDepthError', 'ShapeError', 'RunStopException']
+    'setup_logging',
+    'FocusStackError', 'InvalidOptionError', 'ImageLoadError', 'ImageSaveError',
+    'AlignmentError','BitDepthError', 'ShapeError', 'RunStopException',
+    'Job']
