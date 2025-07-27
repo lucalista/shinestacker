30db3809286504a2b34ca032bb1e05b9a7e112c8
Luca Lista <luca.lista@infn.it>
2025-07-27 17:11:31 +0200

    enabled mathjax [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,10 @@ project = 'Shine Stacker'
 author = 'Luca Lista'
 html_title = "Shine Stacker Documentation"
 
-extensions = ['myst_parser']
+extensions = [
+    'myst_parser',
+    'sphinx.ext.mathjax'
+]
 
 source_suffix = {
     '.rst': 'restructuredtext',
