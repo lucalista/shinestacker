8403ed7127c3847da56dd8b3d5a88a571bde1844
Luca Lista <luca.lista@infn.it>
2025-07-27 19:02:01 +0200

    fixed exports

--- src/shinestacker/__init__.py
+++ src/shinestacker/__init__.py
@@ -3,11 +3,14 @@ from ._version import __version__
 from . import config
 from . import core
 from . import algorithms
+from .config import __all__
+from .core import __all__
+from .algorithms import __all__
 from .config import *
 from .core import *
 from .algorithms import *
 
 __all__ = ['__version__']
-__all__ += config.__all__
-__all__ += core.__all__
-__all__ += algorithms.__all__
+#__all__ += config.__all__
+#__all__ += core.__all__
+#__all__ += algorithms.__all__
