0dd86eac6fec285ba36765253eb7320669e1a460
Luca Lista <luca.lista@infn.it>
2025-07-28 14:20:25 +0200

    update unit test

--- tests/test_0090_vignetting.py
+++ tests/test_0090_vignetting.py
@@ -1,3 +1,5 @@
+import matplotlib
+matplotlib.use('Agg')
 from shinestacker.algorithms.stack_framework import StackJob, CombinedActions
 from shinestacker.algorithms.vignetting import Vignetting
 
@@ -6,7 +8,7 @@ def test_vignetting():
     try:
         job = StackJob("job", "examples", input_path="input/img-vignetted")
         job.add_action(CombinedActions("vignette",
-                                       [Vignetting(plot_histograms=True)],
+                                       [Vignetting(plot_correction=True, plot_summary=True)],
                                        output_path="output/img-vignetting"))
         job.run()
     except Exception:
