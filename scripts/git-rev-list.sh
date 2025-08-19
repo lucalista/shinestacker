29da7d52c8f54bdff190b3fdeeb8f55d7c5b339f
Luca Lista <luca.lista@infn.it>
2025-08-19 15:26:59 +0200

    updated script [ci skip]

--- scripts/git-rev-list.sh
+++ scripts/git-rev-list.sh
@@ -3,4 +3,4 @@ git rev-list --objects --all |
   git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' |
   awk '/^blob/ {print $3,$4}' |
   sort -n -k1 |
-  tail -20
+  tail -50
