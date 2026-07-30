57ab7d576ede32e9294b2b9507a041832964cc80
Luca Lista <luca.lista@na.infn.it>
2026-07-30 16:02:23 +0200

    fixed autoretouch option (#56) [ci skip]

--- src/shinestacker/app/args_parser_opts.py
+++ src/shinestacker/app/args_parser_opts.py
@@ -17,11 +17,11 @@ input folder path for new project.
 Automatically run scheduled jobs at startup.
 ''')
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-e', '--enable-auto-retouch', action='store_true',
+    group.add_argument('-e', '--enable-auto-retouch', action='store_const', const=True, dest='auto_retouch',
                        help='''
 Enable retouch after run at startup''')
-    group.add_argument('-d', '--disable-auto-retouch', action='store_false',
-                       dest='auto_retouch', help='''
+    group.add_argument('-d', '--disable-auto-retouch', action='store_const', const=False, dest='auto_retouch',
+                       help='''
 Disable retouch after run at startup''')
     parser.set_defaults(auto_retouch=None)
 
